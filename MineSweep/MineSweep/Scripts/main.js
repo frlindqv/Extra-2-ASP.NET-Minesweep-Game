8a8761aa5450cc4f94ba4cf97712aa5bf5f57623
frlindqv <frlindqv@kth.se>
2016-02-17 11:11:04 +0100

    JS OOP Classes Player and MineSweep added

--- MineSweep/MineSweep/Scripts/main.js
+++ MineSweep/MineSweep/Scripts/main.js
@@ -1,9 +1,62 @@
-﻿$(document).ready(function ()
+﻿var mineSweep = new MineSweep();
+
+
+$(document).ready(function ()
 {
     $('.carousel').carousel({
         interval: 7500
     });
 
-   
+
+    mineSweep.Player().SetName("Fredrik"); 
+    mineSweep.Player().IncreaseClicks();
+
+    //mineSweep.SetPlayer(new Player("Fredrik"));
+
+    //mineSweep.InitGame();
+    $("h1").click(function () {
+        console.log(mineSweep.Player().Clicks());
+    });
 
 });
+
+
+
+function MineSweep(){
+
+   var player = new Player();
+
+   this.InitGame = function () {};
+
+    this.RunGame = function () { };
+
+    this.EndGame = function () { };
+
+
+    this.Player = function () {
+        return player;
+    }
+
+    this.SetPlayer = function (newPlayer) {
+        player = newPlayer;
+    }
+
+
+}
+
+
+function Player() {
+
+    var name;
+    var time;
+    var clicks = 0;
+
+    this.Name = function (){ return name; };
+
+    this.SetName = function (val) { name = val; };
+
+    this.Clicks = function () { return clicks; };
+
+    this.IncreaseClicks = function () { clicks++; };
+
+}
